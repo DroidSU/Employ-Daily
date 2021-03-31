89b272abce00e011d9198a641f428d443e842bf8
Sujoy Datta <sujoydatta26@gmail.com>
2021-03-31 12:04:14 +0530

    Initial commit

---
+++ app/src/main/java/com/brixham/employdaily/views/MainActivity.java
@@ -0,0 +1,15 @@
+package com.brixham.employdaily.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.brixham.employdaily.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
