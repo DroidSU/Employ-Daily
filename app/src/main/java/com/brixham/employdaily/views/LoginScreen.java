c7a5ea719efacc87ed2172799178e02cf95ecdad
kabir376 <70939526+kabir376@users.noreply.github.com>
2021-04-01 16:22:24 +0530

    registrationPage 01-04-2021

--- app/src/main/java/com/brixham/employdaily/views/LoginScreen.java
+++ app/src/main/java/com/brixham/employdaily/views/LoginScreen.java
@@ -4,17 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import com.brixham.employdaily.R;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.brixham.employdaily.R;
 
 public class LoginScreen extends AppCompatActivity {
 
+    private TextView registerText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DataBindingUtil.setContentView(this, R.layout.activity_login_screen);
+        registerText = findViewById(R.id.textViewRegister);
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginScreen.this, RegistrationPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/brixham/employdaily/views/RegistrationPage.java
+++ app/src/main/java/com/brixham/employdaily/views/RegistrationPage.java
@@ -1,17 +1,30 @@
 package com.brixham.employdaily.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.brixham.employdaily.R;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.brixham.employdaily.R;
 
 public class RegistrationPage extends AppCompatActivity {
 
+    private Spinner spinner;
+    private Adapter adapter;
+    String[] items = new String[]{"Male", "Female", "Others"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+        spinner = findViewById(R.id.registerGender_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(RegistrationPage.this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinner.setAdapter(adapter);
+
     }
+
 }
