382cc1dab24e61d1860594b0d5955980b96c39dd
kabir376 <70939526+kabir376@users.noreply.github.com>
2021-04-01 10:47:00 +0530

    ed-login 01.04.2021

--- app/src/main/java/com/brixham/employdaily/views/LoginScreen.java
+++ app/src/main/java/com/brixham/employdaily/views/LoginScreen.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.brixham.employdaily.R;
-
-public class SplashActivity extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_login_screen);
     }
 }
--- app/src/main/java/com/brixham/employdaily/views/RegistrationPage.java
+++ app/src/main/java/com/brixham/employdaily/views/RegistrationPage.java
@@ -1,15 +1,14 @@
 package com.brixham.employdaily.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import com.brixham.employdaily.R;
-
-public class MainActivity extends AppCompatActivity {
+public class RegistrationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration_page);
     }
 }
